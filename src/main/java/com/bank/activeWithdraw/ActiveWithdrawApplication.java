2288294ecfc9868530c1482e65f79dfd4970ca73
Rodrigo Gonzales <rgonzalesm1997@gmail.com>
2022-02-11 16:16:35 -0500

    first commit

---
+++ src/main/java/com/bank/activeWithdraw/ActiveWithdrawApplication.java
@@ -0,0 +1,13 @@
+package com.bank.activeWithdraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveWithdrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveWithdrawApplication.class, args);
+	}
+
+}
